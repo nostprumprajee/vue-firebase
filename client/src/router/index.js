3de6e7cb043af9bb1d86d6e4054b5d6808f3544f
nostprumprajee <nostprumprajee@gmail.com>
2019-01-30 18:27:25 +0700

    MEVNN

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Hello from '@/components/Hello'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
